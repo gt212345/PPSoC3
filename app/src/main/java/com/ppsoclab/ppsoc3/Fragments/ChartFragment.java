4cc67a7a13958759264b320a2edc0b70e30e1055
Heiru Wu <wuheiru.5203@gmail.com>
2015-05-11 20:03:14 +0800

    graphic error

--- app/src/main/java/com/ppsoclab/ppsoc3/Fragments/ChartFragment.java
+++ app/src/main/java/com/ppsoclab/ppsoc3/Fragments/ChartFragment.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
@@ -90,14 +92,20 @@ public class ChartFragment extends Fragment implements DataListener{
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        chartInit();
         init = new ArrayList<>();
-        init.add(x1angle);init.add(x2angle);init.add(x3angle);init.add(x4angle);init.add(x5angle);init.add(x6angle);init.add(x7angle);init.add(x8angle);
-        init.add(y1angle);init.add(y2angle);init.add(y3angle);init.add(y4angle);init.add(y5angle);init.add(y6angle);init.add(y7angle);init.add(y8angle);
-        init.add(z1angle);init.add(z2angle);init.add(z3angle);init.add(z4angle);init.add(z5angle);init.add(z6angle);init.add(z7angle);init.add(z8angle);
-        init.add(x1raw);init.add(x2raw);init.add(x3raw);init.add(x4raw);init.add(x5raw);init.add(x6raw);init.add(x7raw);init.add(x8raw);
-        init.add(y1raw);init.add(y2raw);init.add(y3raw);init.add(y4raw);init.add(y5raw);init.add(y6raw);init.add(y7raw);init.add(y8raw);
-        init.add(z1raw);init.add(z2raw);init.add(z3raw);init.add(z4raw);init.add(z5raw);init.add(z6raw);init.add(z7raw);init.add(z8raw);
-        arrayInit();
+        x1angle = new ArrayList<>();x2angle = new ArrayList<>();x3angle = new ArrayList<>();x4angle = new ArrayList<>();
+        x5angle = new ArrayList<>();x6angle = new ArrayList<>();x7angle = new ArrayList<>();x8angle = new ArrayList<>();
+        y1angle = new ArrayList<>();y2angle = new ArrayList<>();y3angle = new ArrayList<>();y4angle = new ArrayList<>();
+        y5angle = new ArrayList<>();y6angle = new ArrayList<>();y7angle = new ArrayList<>();y8angle = new ArrayList<>();
+        z1angle = new ArrayList<>();z2angle = new ArrayList<>();z3angle = new ArrayList<>();z4angle = new ArrayList<>();
+        z5angle = new ArrayList<>();z6angle = new ArrayList<>();z7angle = new ArrayList<>();z8angle = new ArrayList<>();
+        x1raw = new ArrayList<>();x2raw = new ArrayList<>();x3raw = new ArrayList<>();x4raw = new ArrayList<>();
+        x5raw = new ArrayList<>();x6raw = new ArrayList<>();x7raw = new ArrayList<>();x8raw = new ArrayList<>();
+        y1raw = new ArrayList<>();y2raw = new ArrayList<>();y3raw = new ArrayList<>();y4raw = new ArrayList<>();
+        y5raw = new ArrayList<>();y6raw = new ArrayList<>();y7raw = new ArrayList<>();y8raw = new ArrayList<>();
+        z1raw = new ArrayList<>();z2raw = new ArrayList<>();z3raw = new ArrayList<>();z4raw = new ArrayList<>();
+        z5raw = new ArrayList<>();z6raw = new ArrayList<>();z7raw = new ArrayList<>();z8raw = new ArrayList<>();
         handlerThread = new HandlerThread("");
         handlerThread.start();
         handler = new Handler(handlerThread.getLooper());
@@ -105,89 +113,86 @@ public class ChartFragment extends Fragment implements DataListener{
             @Override
             public void run() {
                 while (true){
-                    if(x1angle.size() >= 500){
+                    if(x1angle!=null&&x1angle.size() >= 100){
                         chartDraw();
                     }
                 }
             }
         });
-        lineChart = (LineChart)getView().findViewById(R.id.chart);
-        chartInit();
+
     }
 
     @Override
     public void onDataReceived(byte[] data) {
-        x1angle.add(ByteParse.sIN16From2Byte(data[2],data[3]));
-        y1angle.add(ByteParse.sIN16From2Byte(data[4],data[5]));
-        z1angle.add(ByteParse.sIN16From2Byte(data[6],data[7]));
-        x1raw.add(ByteParse.sIN16From2Byte(data[8],data[9]));
-        y1raw.add(ByteParse.sIN16From2Byte(data[10],data[11]));
-        z1raw.add(ByteParse.sIN16From2Byte(data[12],data[13]));
+        if(x1angle!=null) {
+            x1angle.add(ByteParse.sIN16From2Byte(data[2], data[3]));
+            y1angle.add(ByteParse.sIN16From2Byte(data[4], data[5]));
+            z1angle.add(ByteParse.sIN16From2Byte(data[6], data[7]));
+            x1raw.add(ByteParse.sIN16From2Byte(data[8], data[9]));
+            y1raw.add(ByteParse.sIN16From2Byte(data[10], data[11]));
+            z1raw.add(ByteParse.sIN16From2Byte(data[12], data[13]));
 
-        x2angle.add(ByteParse.sIN16From2Byte(data[14],data[15]));
-        y2angle.add(ByteParse.sIN16From2Byte(data[16],data[17]));
-        z2angle.add(ByteParse.sIN16From2Byte(data[18],data[19]));
-        x2raw.add(ByteParse.sIN16From2Byte(data[20],data[21]));
-        y2raw.add(ByteParse.sIN16From2Byte(data[22],data[23]));
-        z2raw.add(ByteParse.sIN16From2Byte(data[24],data[25]));
+            x2angle.add(ByteParse.sIN16From2Byte(data[14], data[15]));
+            y2angle.add(ByteParse.sIN16From2Byte(data[16], data[17]));
+            z2angle.add(ByteParse.sIN16From2Byte(data[18], data[19]));
+            x2raw.add(ByteParse.sIN16From2Byte(data[20], data[21]));
+            y2raw.add(ByteParse.sIN16From2Byte(data[22], data[23]));
+            z2raw.add(ByteParse.sIN16From2Byte(data[24], data[25]));
 
-        x3angle.add(ByteParse.sIN16From2Byte(data[26],data[27]));
-        y3angle.add(ByteParse.sIN16From2Byte(data[28],data[29]));
-        z3angle.add(ByteParse.sIN16From2Byte(data[30],data[31]));
-        x3raw.add(ByteParse.sIN16From2Byte(data[32],data[33]));
-        y3raw.add(ByteParse.sIN16From2Byte(data[34],data[35]));
-        z3raw.add(ByteParse.sIN16From2Byte(data[36], data[37]));
+            x3angle.add(ByteParse.sIN16From2Byte(data[26], data[27]));
+            y3angle.add(ByteParse.sIN16From2Byte(data[28], data[29]));
+            z3angle.add(ByteParse.sIN16From2Byte(data[30], data[31]));
+            x3raw.add(ByteParse.sIN16From2Byte(data[32], data[33]));
+            y3raw.add(ByteParse.sIN16From2Byte(data[34], data[35]));
+            z3raw.add(ByteParse.sIN16From2Byte(data[36], data[37]));
 
-        x4angle.add(ByteParse.sIN16From2Byte(data[38],data[39]));
-        y4angle.add(ByteParse.sIN16From2Byte(data[40],data[41]));
-        z4angle.add(ByteParse.sIN16From2Byte(data[42],data[43]));
-        x4raw.add(ByteParse.sIN16From2Byte(data[44],data[45]));
-        y4raw.add(ByteParse.sIN16From2Byte(data[46],data[47]));
-        z4raw.add(ByteParse.sIN16From2Byte(data[48],data[49]));
+            x4angle.add(ByteParse.sIN16From2Byte(data[38], data[39]));
+            y4angle.add(ByteParse.sIN16From2Byte(data[40], data[41]));
+            z4angle.add(ByteParse.sIN16From2Byte(data[42], data[43]));
+            x4raw.add(ByteParse.sIN16From2Byte(data[44], data[45]));
+            y4raw.add(ByteParse.sIN16From2Byte(data[46], data[47]));
+            z4raw.add(ByteParse.sIN16From2Byte(data[48], data[49]));
 
-        x5angle.add(ByteParse.sIN16From2Byte(data[50],data[51]));
-        y5angle.add(ByteParse.sIN16From2Byte(data[52],data[53]));
-        z5angle.add(ByteParse.sIN16From2Byte(data[54],data[55]));
-        x5raw.add(ByteParse.sIN16From2Byte(data[56],data[57]));
-        y5raw.add(ByteParse.sIN16From2Byte(data[58],data[59]));
-        z5raw.add(ByteParse.sIN16From2Byte(data[60],data[61]));
+            x5angle.add(ByteParse.sIN16From2Byte(data[50], data[51]));
+            y5angle.add(ByteParse.sIN16From2Byte(data[52], data[53]));
+            z5angle.add(ByteParse.sIN16From2Byte(data[54], data[55]));
+            x5raw.add(ByteParse.sIN16From2Byte(data[56], data[57]));
+            y5raw.add(ByteParse.sIN16From2Byte(data[58], data[59]));
+            z5raw.add(ByteParse.sIN16From2Byte(data[60], data[61]));
 
-        x6angle.add(ByteParse.sIN16From2Byte(data[62],data[63]));
-        y6angle.add(ByteParse.sIN16From2Byte(data[64],data[65]));
-        z6angle.add(ByteParse.sIN16From2Byte(data[66],data[67]));
-        x6raw.add(ByteParse.sIN16From2Byte(data[68],data[69]));
-        y6raw.add(ByteParse.sIN16From2Byte(data[70],data[71]));
-        z6raw.add(ByteParse.sIN16From2Byte(data[72],data[73]));
+            x6angle.add(ByteParse.sIN16From2Byte(data[62], data[63]));
+            y6angle.add(ByteParse.sIN16From2Byte(data[64], data[65]));
+            z6angle.add(ByteParse.sIN16From2Byte(data[66], data[67]));
+            x6raw.add(ByteParse.sIN16From2Byte(data[68], data[69]));
+            y6raw.add(ByteParse.sIN16From2Byte(data[70], data[71]));
+            z6raw.add(ByteParse.sIN16From2Byte(data[72], data[73]));
 
-        x7angle.add(ByteParse.sIN16From2Byte(data[74],data[75]));
-        y7angle.add(ByteParse.sIN16From2Byte(data[76],data[77]));
-        z7angle.add(ByteParse.sIN16From2Byte(data[78],data[79]));
-        x7raw.add(ByteParse.sIN16From2Byte(data[80],data[81]));
-        y7raw.add(ByteParse.sIN16From2Byte(data[82],data[83]));
-        z7raw.add(ByteParse.sIN16From2Byte(data[84],data[85]));
+            x7angle.add(ByteParse.sIN16From2Byte(data[74], data[75]));
+            y7angle.add(ByteParse.sIN16From2Byte(data[76], data[77]));
+            z7angle.add(ByteParse.sIN16From2Byte(data[78], data[79]));
+            x7raw.add(ByteParse.sIN16From2Byte(data[80], data[81]));
+            y7raw.add(ByteParse.sIN16From2Byte(data[82], data[83]));
+            z7raw.add(ByteParse.sIN16From2Byte(data[84], data[85]));
 
-        x8angle.add(ByteParse.sIN16From2Byte(data[86],data[87]));
-        y8angle.add(ByteParse.sIN16From2Byte(data[88],data[89]));
-        z8angle.add(ByteParse.sIN16From2Byte(data[90],data[91]));
-        x8raw.add(ByteParse.sIN16From2Byte(data[92],data[93]));
-        y8raw.add(ByteParse.sIN16From2Byte(data[94],data[95]));
-        z8raw.add(ByteParse.sIN16From2Byte(data[96],data[97]));
-    }
-
-    private void arrayInit(){
-        for (ArrayList temp : init) {
-            temp = new ArrayList();
+            x8angle.add(ByteParse.sIN16From2Byte(data[86], data[87]));
+            y8angle.add(ByteParse.sIN16From2Byte(data[88], data[89]));
+            z8angle.add(ByteParse.sIN16From2Byte(data[90], data[91]));
+            x8raw.add(ByteParse.sIN16From2Byte(data[92], data[93]));
+            y8raw.add(ByteParse.sIN16From2Byte(data[94], data[95]));
+            z8raw.add(ByteParse.sIN16From2Byte(data[96], data[97]));
         }
     }
 
     private void chartInit() {
+        lineChart = (LineChart)getView().findViewById(R.id.chart);
         lineChart.setDescription("");
+        lineChart.setNoDataText("");
         lineChart.setHighlightEnabled(true);
         lineChart.setTouchEnabled(true);
 //        lineChart.setDragDecelerationFrictionCoef(0.95f);
         lineChart.setScaleEnabled(true);
         lineChart.setDrawGridBackground(false);
-        lineChart.setBackgroundColor(Color.LTGRAY);
+        lineChart.setBackgroundColor(Color.WHITE);
     }
 
     private void chartDraw() {
@@ -207,36 +212,36 @@ public class ChartFragment extends Fragment implements DataListener{
         lineDataSet1.setAxisDependency(YAxis.AxisDependency.LEFT);
         lineDataSet1.setColor(ColorTemplate.getHoloBlue());
         lineDataSet1.setCircleColor(Color.WHITE);
-        lineDataSet1.setLineWidth(2f);
-        lineDataSet1.setCircleSize(3f);
-        lineDataSet1.setDrawCubic(true);
+//        lineDataSet1.setLineWidth(2f);
+//        lineDataSet1.setCircleSize(3f);
+//        lineDataSet1.setDrawCubic(true);
         LineDataSet lineDataSet2 = new LineDataSet(yData2,"Y4ANGEL");
         lineDataSet2.setAxisDependency(YAxis.AxisDependency.LEFT);
         lineDataSet2.setColor(Color.RED);
         lineDataSet2.setCircleColor(Color.WHITE);
-        lineDataSet2.setLineWidth(2f);
-        lineDataSet2.setCircleSize(3f);
-        lineDataSet2.setDrawCubic(true);
+//        lineDataSet2.setLineWidth(2f);
+//        lineDataSet2.setCircleSize(3f);
+//        lineDataSet2.setDrawCubic(true);
         LineDataSet lineDataSet3 = new LineDataSet(yData3,"Z4ANGEL");
         lineDataSet3.setAxisDependency(YAxis.AxisDependency.LEFT);
         lineDataSet3.setColor(Color.GREEN);
         lineDataSet3.setCircleColor(Color.WHITE);
-        lineDataSet3.setLineWidth(2f);
-        lineDataSet3.setCircleSize(3f);
-        lineDataSet3.setDrawCubic(true);
+//        lineDataSet3.setLineWidth(2f);
+//        lineDataSet3.setCircleSize(3f);
+//        lineDataSet3.setDrawCubic(true);
         ArrayList<LineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(lineDataSet1);dataSets.add(lineDataSet2);dataSets.add(lineDataSet3);
-        ArrayList<String> xVals = new ArrayList<String>();
-        /**
-                 * X¶b°Ñ¼Æ
-                */
-        for(int i = 0;i < yData1.size();i++){
+        dataSets.add(lineDataSet1);
+//        dataSets.add(lineDataSet2);dataSets.add(lineDataSet3);
+        ArrayList<String> xVals = new ArrayList<>();
+        for(int i = 0;i < x4angle.size();i++) {
             xVals.add((0.1*i)+" s");
         }
         LineData data = new LineData(xVals,dataSets);
+        data.setValueTextColor(Color.WHITE);
+        data.setValueTextSize(9f);
         lineChart.setData(data);
-        Legend l = lineChart.getLegend();
-        l.setForm(Legend.LegendForm.LINE);
         lineChart.animateX(2000);
+//        Legend l = lineChart.getLegend();
+//        l.setForm(Legend.LegendForm.LINE);
     }
 }
